089d335826d665c5464b5118785d6f39c77ef8a5
eandualem <eandualem@gmail.com>
2021-03-10 10:32:07 +0300

    Added chart view to the app.

--- lib/ui/views/home/chart.dart
+++ lib/ui/views/home/chart.dart
@@ -1,4 +1,5 @@
 import 'package:expense_aid/core/models/transaction.dart';
+import 'package:expense_aid/ui/views/home/char_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -22,16 +23,31 @@ class Chart extends StatelessWidget {
       return {'day': DateFormat.E().format(weekDay).substring(0, 1), 'amount': totalSum};
     });
   }
+
+  double get totalSpending{
+    return groupedTransactionValues.fold(0.00, (sum, item) => sum+item['amount']);
+  }
   @override
   Widget build(BuildContext context) {
     print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return Text("${data["day"]}  ${data["amount"].toString()}");
-        }).toList(),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            // return Text("${data["day"]}  ${data["amount"].toString()}");
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                  data["day"],
+                  data["amount"],
+                  totalSpending == 0.0 ? 0.0 : (data["amount"] as double)/totalSpending),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
